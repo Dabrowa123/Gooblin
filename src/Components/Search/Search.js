46ebb982e2eb4c0eceb530223a36030294cca4f4
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-10-03 18:09:03 +0200

    adding ticket list

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -2,7 +2,47 @@ import React from "react";
 import "./Search.css";
 
 function Search() {
-  return <div></div>;
+  return (
+    <div>
+      <ol class="list-group list-group-numbered">
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Ticket Name</div>
+            Description
+          </div>
+          <span class="badge bg-status-active rounded-pill">Active</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Ticket Name</div>
+            Description
+          </div>
+          <span class="badge bg-status-active rounded-pill">Active</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Ticket Name</div>
+            Description
+          </div>
+          <span class="badge bg-status-active rounded-pill">Active</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Ticket Name</div>
+            Description
+          </div>
+          <span class="badge bg-status-active rounded-pill">Active</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Ticket Name</div>
+            Description
+          </div>
+          <span class="badge bg-status-closed rounded-pill">Closed</span>
+        </li>
+      </ol>
+    </div>
+  );
 }
 
 export default Search;
