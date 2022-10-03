62d07e669bbcef6a37e96ff4944220a070309508
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-10-03 13:45:55 +0200

    creating react app & simple component structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./Components/Nav/Nav.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Nav />
+
+        <main>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Search />} />
+              <Route path="/login" element={<LoginView />} />
+              <Route path="/addticket" element={<AddTicket />} />
+              <Route path="/viewticket" element={<ViewTicket />} />
+            </Routes>
+          </div>
+        </main>
+
+        <footer>
+          <div className="container"></div>
+        </footer>
+      </Router>
+    </>
   );
 }
 
