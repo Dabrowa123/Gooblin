a2b35e6abc09df7ca2d4ebc194315126f04b4f28
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-10-03 21:35:54 +0200

    adding search bar & refactoring button's icons

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -12,15 +12,13 @@ function Nav() {
           className="btn btn-login btn-dark btn-lg"
           data-bs-toggle="button"
         >
-          Log In
-          <img className="icon-login" src={icon.login} alt="login icon" />
+          Log In <i class="bi bi-box-arrow-in-right"></i>
         </button>
         <button
           type="button"
           className="btn btn-add-ticket btn-outline-dark btn-lg"
         >
-          Create Ticket
-          <img className="icon-plus" src={icon.plus} alt="plus icon" />
+          Create Ticket <i class="bi bi-plus-circle"></i>
         </button>
       </div>
     </nav>
