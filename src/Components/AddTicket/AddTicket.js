b135bdd0db1314106f709447aa7184a85c7cb414
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-10-04 18:08:23 +0200

    refactoring login section layoutn

--- src/Components/AddTicket/AddTicket.js
+++ src/Components/AddTicket/AddTicket.js
@@ -2,7 +2,42 @@ import React from "react";
 import "./AddTicket.css";
 
 function AddTicket() {
-  return <div></div>;
+  return (
+    <div className="addTicket">
+      <h2>Create Ticket</h2>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Add Ticket Name"
+          aria-label="Write Ticket Name"
+        />
+      </div>
+      <div class="input-group">
+        <textarea
+          class="form-control"
+          aria-label="Write Ticket Description"
+          placeholder="Add Ticket Description"
+        ></textarea>
+      </div>
+      <div className="addTicket__buttons-area">
+        <button
+          type="button"
+          className="btn btn-theme-2 btn-dark btn-lg"
+          data-bs-toggle="button"
+        >
+          Save
+        </button>
+        <button
+          type="button"
+          className="btn btn-theme-1 btn-dark btn-lg"
+          data-bs-toggle="button"
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default AddTicket;
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,40 +1,8 @@
 import React from "react";
 import "./Login.css";
 
-function Link() {
-  return (
-    <div>
-      <button
-        type="button"
-        className="btn btn-theme-2 btn-dark btn-lg"
-        data-bs-toggle="button"
-      >
-        Save
-      </button>
-      <button
-        type="button"
-        className="btn btn-theme-1 btn-dark btn-lg"
-        data-bs-toggle="button"
-      >
-        Cancel
-      </button>
-      <div class="input-group mb-3">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Ticket Name"
-          aria-label="Write Ticket Name"
-        />
-      </div>
-      <div class="input-group">
-        <textarea
-          class="form-control"
-          aria-label="Write Ticket Description"
-          placeholder="Ticket Description"
-        ></textarea>
-      </div>
-    </div>
-  );
+function Login() {
+  return <div></div>;
 }
 
-export default Link;
+export default Login;
